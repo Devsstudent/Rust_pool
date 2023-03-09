c1b0b8b5833ebaddc18bccd26fd1062f7ed34df3
Devsstudent <devsstudent777@gmail.com>
2023-03-09 21:43:14 +0100

    ex05 added

---
+++ Rust00/ex05/target/debug/deps/hello_exe-53455b7456c2efcc.d
@@ -0,0 +1,5 @@
+/home/hanako/Project/RUST/Rust00/ex05/target/debug/deps/hello_exe-53455b7456c2efcc: src/main.rs
+
+/home/hanako/Project/RUST/Rust00/ex05/target/debug/deps/hello_exe-53455b7456c2efcc.d: src/main.rs
+
+src/main.rs:
---
+++ Rust00/ex05/target/debug/hello_exe.d
@@ -0,0 +1,1 @@
+/home/hanako/Project/RUST/Rust00/ex05/target/debug/hello_exe: /home/hanako/Project/RUST/Rust00/ex05/src/main.rs
